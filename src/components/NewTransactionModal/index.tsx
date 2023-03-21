4fd9a4b24232eecae39bc4092996826924b68291
Mateus Garcia <mateussk83@gmail.com>
2023-03-21 11:50:48 -0300

    feat: add modal from new Transaction

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,15 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
+export function NewTransactionModal() {
+ return (
+  <Dialog.Portal>
+        <Dialog.Overlay>
+          <Dialog.Content>
+
+          <Dialog.Title>Nova Transação</Dialog.Title>
+          <Dialog.Close />
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+ )
+}
